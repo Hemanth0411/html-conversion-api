7af217652b39899bb7d67f7ad9a9af8693a6df35
Hemanth0411 <hemanthreddyannem@gmail.com>
2025-10-25 13:13:49 +0530

    feat: add readme and implement word conversion endpoint

--- src/routes/conversionRoutes.js
+++ src/routes/conversionRoutes.js
@@ -4,5 +4,6 @@ const conversionController = require('../controllers/conversionController');
 
 // Define the POST route for PDF conversion
 router.post('/pdf', conversionController.convertToPdf);
+router.post('/word', conversionController.convertToDocx);
 
 module.exports = router;
